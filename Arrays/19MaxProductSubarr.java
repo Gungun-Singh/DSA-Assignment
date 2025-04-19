b413761d0b1a306ca3a5e7acfac9f8e0c4dbe1ca
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2025-04-19 18:44:58 +0530

    19MaxProductSubarr.java

--- Arrays/19MaxProductSubarr.java
+++ Arrays/19MaxProductSubarr.java
@@ -2,41 +2,23 @@
 and space complexities. */
 
 
-public class MaxProductSubarray {
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxPro = Integer.MIN_VALUE;
 
-    public static int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-
-        int maxProduct = nums[0];
-        int currMax = nums[0];
-        int currMin = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            int temp = currMax;
-
-            if (nums[i] < 0) {
-                // Swap currMax and currMin if current element is negative
-                currMax = currMin;
-                currMin = temp;
+        for (int i =0;i<nums.length;i++) {
+            int product = 1;
+            for (int j =i;j <nums.length;j++) {
+                product *=nums[j];
+                maxPro =Math.max(maxPro, product);
             }
-
-            currMax = Math.max(nums[i], currMax * nums[i]);
-            currMin = Math.min(nums[i], currMin * nums[i]);
-
-            maxProduct = Math.max(maxProduct, currMax);
         }
 
-        return maxProduct;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {2, 3, -2, 4};
-        int result = maxProduct(nums);
-        System.out.println("Maximum Product Subarray: " + result);
+        return maxPro; 
     }
 }
 
 
 
-//TC - O(n)
+//TC - O(n^2)
 //SC - O(1)
