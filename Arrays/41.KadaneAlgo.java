adf4ce653d5fbbdf1b3dc3b29f4eb1179f995355
Gungun-Singh <144252652+Gungun-Singh@users.noreply.github.com>
2025-04-25 11:20:19 +0530

    41.KadaneAlgo.java

--- Arrays/41.KadaneAlgo.java
+++ Arrays/41.KadaneAlgo.java
@@ -3,26 +3,29 @@
 
 public class MaximumSubarraySum {
 
-    // Function to find the maximum subarray sum using Kadane's Algorithm
-    public static int maxSubArraySum(int[] nums) {
-        int currentMax = nums[0];  // Initialize current max to the first element
-        int globalMax = nums[0];   // Initialize global max to the first element
-        
-        // Loop through the array starting from the second element
-        for (int i = 1; i < nums.length; i++) {
-            // Update currentMax
-            currentMax = Math.max(nums[i], currentMax + nums[i]);
-            
-            // Update globalMax
-            globalMax = Math.max(globalMax, currentMax);
-        }
+     public int maxSubArray(int[] nums) {
+        int n = nums.length;
 
-        return globalMax;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if (sum > max) {
+                max = sum;
+
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println("Maximum subarray sum: " + maxSubArraySum(nums));
+        System.out.println("Maximum subarray sum: " + maxSubArray(nums));
     }
 }
 
